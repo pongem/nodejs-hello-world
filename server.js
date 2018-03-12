05b17454f9d6a56284e8aad94fc8e75c26969992
Pongsak <pongsak.s@gmail.com>
2018-03-12 16:41:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ var app=http.createServer(function (req, res) {
   //outputs string with line end symbol
 }).listen(8080, "0.0.0.0");
 //sets port and IP address of the server
-console.log('Server running at http://127.0.0.1:1337/');
+console.log('Server running at http://0.0.0.0:8080/');
